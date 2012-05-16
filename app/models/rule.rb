46a49eb30a713357600f9f5ae99cdb07f5b83c0e
Andrew Bills <andrew.bills@mincom.com>
2012-05-16 10:47:47 +1000

    resubmitting to ensure all files for engineyard deploy test

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,4 +1,77 @@
 class Rule < ActiveRecord::Base
   attr_accessible :title, :group_id, :group_owner, :source, :cust_no, :call_type, :priority, :entitlement_code, :milestone1_operator, :milestone1_value, :milestone1_time_value, :milestone1_time_value_denomination, :target_time_operator, :target_time_value, :target_time_value_denomination, :ctc_id_operator, :ctc_id_value, :sql_query, :syntax_msg
   belongs_to :group
+  before_save :build_sql_query
+
+  def build_sql_query
+    @rule = Rule.find(params[:id])
+
+    db_name = "db_name"
+    table_name = "table_name"
+
+    temp_sql = "select `#{db_name}`.`#{table_name}`.* from `#{db_name}`.`#{table_name}` where "
+    #add limits
+    #add source to sql statement
+    if not @rule.source.blank?
+      temp_sql.concat("source =\"#{@rule.source}\"")
+    end
+    #add customer no to sql statement
+    if not @rule.cust_no.blank?
+      temp_sql.concat(" and cust_no =\"#{@rule.cust_no}\"")
+    end
+    #add call_type to sql statement
+    if @rule.call_type != "NULL"
+      temp_sql.concat(" and call_type =\"#{@rule.call_type}\"")
+    end
+    #add priority to sql statement
+    if @rule.priority != 0
+      temp_sql.concat(" and priority =#{@rule.priority}")
+    end
+    #add group_owner to sql statement
+    if not @rule.group_owner.blank?
+      temp_sql.concat(" and group_owner =\"#{@rule.group_owner}\"")
+    end
+    #add entitlement_code to sql statement
+    if not @rule.entitlement_code.blank?
+      temp_sql.concat(" and entitlement_code =\"#{@rule.entitlement_code}\"")
+    end
+    #add milestone rule condition
+    if @rule.milestone1_operator != "NULL"
+      case @rule.milestone1_operator
+        when "="
+          temp_sql.concat(" and milestone #{@rule.milestone1_operator}\"#{@rule.milestone1_value}\"")
+          #if milestone duration needs to be used
+          if @rule.milestone1_time_value_denomination != "NULL"
+            temp_sql.concat(" and milestone_timestamp >#{@rule.milestone1_time_value}#{@rule.milestone1_time_value_denomination}")
+          end
+        when "count"
+          temp_sql.concat(" !!!add count for milestones!!! ")
+      end
+      #add target time operators
+      if @rule.target_time_operator != "NULL" and @rule.target_time_value_denomination != "NULL"
+        temp_sql.concat(" and target_time #{@rule.target_time_operator}#{@rule.target_time_value}#{@rule.target_time_value_denomination}")
+      end
+      #add CTC rule condition
+      if @rule.ctc_id_operator != "NULL"
+        temp_sql.concat(" and ctc_id #{@rule.ctc_id_operator}\"#{@rule.ctc_id_value}\"")
+      end
+      #add other text rule condition
+      if @rule.other_text_operator != "NULL"
+        case @rule.other_text_operator
+          when "="
+            temp_sql.concat(" and other_text #{@rule.other_text_operator}\"#{@rule.other_text_value}\"")
+          when "!="
+            temp_sql.concat(" and other_text #{@rule.other_text_operator}\"#{@rule.other_text_value}\"")
+          when ">"
+            temp_sql.concat(" and other_text #{@rule.other_text_operator}\"#{@rule.other_text_value}\"")
+          when "<"
+            temp_sql.concat(" and other_text #{@rule.other_text_operator}\"#{@rule.other_text_value}\"")
+          when "count"
+            temp_sql.concat(" !!!add count for other text!!! ")
+        end
+      end
+    end
+
+    @rule.sql_query = temp_sql
+  end
 end
