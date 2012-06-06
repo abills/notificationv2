ecb9be179782023f9ac419f45047a43b7269fedb
Andrew Bills <andrew.bills@mincom.com>
2012-06-06 18:54:12 +1000

    latest running version

--- config/initializers/scheduled_tasks.rb
+++ config/initializers/scheduled_tasks.rb
@@ -9,12 +9,12 @@ Rails.logger.debug "#{Time.now.utc} - Created New Rules Engine"
 n = RulesEngineCaller.new(rule_engine)
 
 #TODO figure out how to not run the next line if doing a rake
-#EngineRulebook.new(rule_engine).rules
+EngineRulebook.new(rule_engine).rules
 Rails.logger.debug "#{Time.now.utc} - Added Rules to New Rules Engine"
 
-scheduler.every('10s', :blocking => true, :allow_overlapping => false) do
+scheduler.every('30s', :blocking => true, :allow_overlapping => false) do
   n.run_engine
 end
-scheduler.every('30s', :blocking => true, :allow_overlapping => false) do
+scheduler.every('60s', :blocking => true, :allow_overlapping => false) do
   n.rebuild_engine
 end
